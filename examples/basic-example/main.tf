e720639a9f70d08a6168fa6a75b2437d50c541ef
Jerome Wolff <Ic3w0lf@users.noreply.github.com>
2023-06-28 13:36:07 +0200

    Example fixes

--- examples/basic-example/main.tf
+++ examples/basic-example/main.tf
@@ -5,6 +5,7 @@ module "vpc" {
   name                = "${var.name}-main"
   cidr                = "10.100.0.0/16"
   azs                 = ["eu-central-1a", "eu-central-1b"]
+  private_subnets     = ["10.100.1.0/24", "10.100.2.0/24"]
   elasticache_subnets = ["10.100.10.0/24", "10.100.11.0/24"]
 }
 
